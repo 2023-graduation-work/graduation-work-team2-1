c36842df74ef68dfc6221c05490cb7bd46502d70
mikami <i.mikami.sys22@morijyobi.ac.jp>
2023-11-08 09:40:25 +0900

    新規登録

--- chat-app/client.py
+++ chat-app/client.py
@@ -21,7 +21,7 @@ def logout():
     root.deiconify()
 
 def login():
-    username = entry_username.get()
+    mail = entry_mail.get()
     password = entry_password.get()
 
     # サーバーに接続
@@ -31,7 +31,7 @@ def login():
     client_socket.connect((server_host, server_port))
 
     # ログイン情報をサーバーに送信
-    login_data = f"{username}:{password}"
+    login_data = f"{mail}:{password}"
     client_socket.send(login_data.encode('utf-8'))
 
     # サーバーからの応答を受信
@@ -44,17 +44,76 @@ def login():
         success_login()
     else:
         messagebox.showerror("ログイン失敗", "ログインに失敗しました。")
+        
+def register():
+    global root,register_page,entry_newusername,entry_newmail,entry_newpassword
+    root.withdraw()
+    register_page = tk.Toplevel()
+    register_page.geometry("400x400")
+    register_page.title("新規アカウント作成")
+    
+    label_newusername = tk.Label(register_page, text="ユーザー名:")
+    label_newusername.pack()
+    entry_newusername = tk.Entry(register_page)
+    entry_newusername.pack()
+    
+    label_newmail = tk.Label(register_page, text="メールアドレス:")  # Changed label text
+    label_newmail.pack()
+    entry_newmail = tk.Entry(register_page)
+    entry_newmail.pack()
 
+    label_newpassword = tk.Label(register_page, text="パスワード:")
+    label_newpassword.pack()
+    entry_newpassword = tk.Entry(register_page, show="*")  # パスワードを隠す
+    entry_newpassword.pack()
+    
+    # Change the command to call the register function
+    register2_button = tk.Button(register_page, text="登録", command=register_user)
+    register2_button.pack()
+
+def register_user():
+    username = entry_newusername.get()
+    mail = entry_newmail.get()  # Get email from entry field
+    password = entry_newpassword.get()
+
+    # サーバーに接続
+    client_socket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+    server_host = 'localhost'  # サーバーのホスト名またはIPアドレス
+    server_port = 12345       # サーバーのポート番号
+    client_socket.connect((server_host, server_port))
+
+    # ユーザー登録情報をサーバーに送信
+    register_data = f"{username}:{mail}:{password}"
+    print(f"register_data: {register_data}")
+    client_socket.send(register_data.encode('utf-8'))
+
+    # サーバーからの応答を受信
+    response = client_socket.recv(1024).decode('utf-8')
+
+    # サーバーとの接続を閉じる
+    client_socket.close()
+
+    if response == "ユーザー登録成功":
+        success_register()
+    else:
+        messagebox.showerror("ユーザー登録失敗", "ユーザー登録に失敗しました。")
+        
+def success_register():
+    global root, register_page, entry_newusername, entry_newmail, entry_newpassword
+    register_page.destroy()
+    root.deiconify()
+    messagebox.showinfo("ユーザー登録成功", "ユーザー登録が成功しました。")
+        
 # Tkinterウィンドウの作成
 root = tk.Tk()
 root.geometry("400x400")
 root.title("ログイン")
 
 # ユーザー名とパスワードの入力フィールド
-label_username = tk.Label(root, text="ユーザー名:")
-label_username.pack()
-entry_username = tk.Entry(root)
-entry_username.pack()
+label_mail = tk.Label(root, text="メールアドレス:")
+label_mail.pack()
+entry_mail = tk.Entry(root)
+entry_mail.pack()
 
 label_password = tk.Label(root, text="パスワード:")
 label_password.pack()
@@ -64,6 +123,9 @@ entry_password.pack()
 # ログインボタン
 login_button = tk.Button(root, text="ログイン", command=login)
 login_button.pack()
+
+register_button = tk.Button(root, text="新規アカウント作成", command=register)
+register_button.pack()
 
 # ウィンドウを実行
 root.mainloop()
